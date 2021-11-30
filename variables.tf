3bb3904228c87c15a3eca24cb73ec9968ed63916
David Arnold <10138997+djaboxx@users.noreply.github.com>
2021-11-30 09:52:21 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,37 +1,37 @@
 variable "vpc_id" {
   description = "VPC ID as assigned by AWS"
-  type        = "string"
+  type        = string
 }
 
 variable "subnet_cidr" {
   default     = "10.0.2.0/24"
   description = "CIDR Mask of private ip address space for subnet"
-  type        = "string"
+  type        = string
 }
 
 variable "public_subnet_id" {
   description = "Public Subnet Id. Used for creating Nat Gateway"
-  type        = "string"
+  type        = string
 }
 
 variable "availability_zone" {
-  type        = "string"
+  type        = string
   description = "Availability zone where subnet will reside"
 }
 
 variable "private_subnet_egress_cidr_block" {
   default     = "0.0.0.0/0"
   description = "block of addresses that are whitelisted for outbound traffic"
-  type        = "string"
+  type        = string
 }
 
 variable "resource_tags" {
   description = "Optional map of tags to set on resources, defaults to empty map."
-  type        = "map"
+  type        = map
   default     = {}
 }
 
 variable "network_name" {
   description = "All ec2 instances that are spun up in this subnet should be associated through having a common purpose. This is a name that specifies that purpose"
-  type        = "string"
+  type        = string
 }
